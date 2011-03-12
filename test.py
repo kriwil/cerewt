68a46436f43853d61a5a076f757c1a46941165d7
Aldiantoro Nugroho <kriwil@gmail.com>
2011-03-12 12:21:05 +0700

    Early twitter test

--- test.py
+++ test.py
@@ -1,5 +1,50 @@
-import tweepy
+from google.appengine.api import users
+from google.appengine.ext import db
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import login_required
+from google.appengine.ext.webapp.util import run_wsgi_app
 
-public_tweets = tweepy.api.public_timeline()
-for tweet in public_tweets:
-    print tweet.text
+import tweepy
+from settings import *
+
+
+class HomeApp(webapp.RequestHandler):
+
+    def get(self):
+
+        #public_tweets = tweepy.api.public_timeline()
+        #for tweet in public_tweets:
+        #    print tweet.text
+
+        callback_url = 'http://localhost:8080/'
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET, 
+                                   callback_url)
+
+        try:
+            self.redirect(auth.get_authorization_url())
+        except tweepy.TweepError, e:
+            print e
+            print 'error! failed to get request token'
+            return
+
+        template_values = {
+            'temp': auth.request_token.key,
+        }
+
+        path = os.path.join(TEMPLATE, 'base.html')
+        self.response.out.write(template.render(path, template_values))
+
+
+application = webapp.WSGIApplication(
+                [
+                  ('/', HomeApp),
+                ],
+                debug=DEBUG
+              )
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
